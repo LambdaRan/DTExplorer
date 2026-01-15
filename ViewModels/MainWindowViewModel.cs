7dba44fdc7303acb460053600f529474c8af8849
LambdaRan <2861429309@qq.com>
2026-01-15 12:52:23 +0800

    add slnx project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DTExplorer.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace DTExplorer.Views
+{
+    public partial class MainWindow : SukiWindow
+	{
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
